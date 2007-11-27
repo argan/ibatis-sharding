c7e497e7e3196c27a2f34a873adf14c5a59491a0
kerrigan.wang <kerrigan.wang@9d485a18-da3e-0410-a38f-e3d2849d2ccb>
2007-11-27 09:34:51 +0000

    fix some thread-safe bug in SumExitStrategy
    git-svn-id: https://ibatis-sharding.googlecode.com/svn@14 9d485a18-da3e-0410-a38f-e3d2849d2ccb

--- trunk/src/java/com/alibaba/china/shard/strategy/access/impl/SumExitStrategy.java
+++ trunk/src/java/com/alibaba/china/shard/strategy/access/impl/SumExitStrategy.java
@@ -1,20 +1,29 @@
 package com.alibaba.china.shard.strategy.access.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.alibaba.china.shard.Shard;
 import com.alibaba.china.shard.strategy.access.ExitOperationsCollector;
 import com.alibaba.china.shard.strategy.access.ExitStrategy;
 
 public class SumExitStrategy implements ExitStrategy {
 
-    private int sum;
+    private Map<Shard, Integer> resultMap = new HashMap<Shard, Integer>();
 
     public boolean addResult(Object result, Shard shard) {
         if (result instanceof Integer)
-            this.sum += (Integer) result;
+            resultMap.put(shard, (Integer) result);
         return false;
     }
 
     public Integer compileResults(ExitOperationsCollector exitOperationsCollector) {
+        int sum = 0;
+        for (Integer count : resultMap.values()) {
+            if (count != null) {
+                sum += count;
+            }
+        }
         return sum;
     }
 
