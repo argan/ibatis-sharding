ad3487fd6af7020ca0173c88b8ce8bd83354029b
kerrigan.wang <kerrigan.wang@9d485a18-da3e-0410-a38f-e3d2849d2ccb>
2007-12-05 09:21:53 +0000

    implement a default simple ResolutionStrategy
    git-svn-id: https://ibatis-sharding.googlecode.com/svn@22 9d485a18-da3e-0410-a38f-e3d2849d2ccb

--- trunk/src/java.test/com/alibaba/china/shard/SimpleDAO.java
+++ trunk/src/java.test/com/alibaba/china/shard/SimpleDAO.java
@@ -6,15 +6,16 @@ import java.util.List;
 import java.util.Map;
 
 import com.alibaba.china.shard.ibatis.ShardedSqlMapClientDAOSupport;
+import com.alibaba.china.shard.strategy.resolution.impl.SimpleResolutionStrategyData;
 
 public class SimpleDAO extends ShardedSqlMapClientDAOSupport {
     public void insertEntity(TestEntity entity) {
-        this.getSqlMapClientTemplate(new LoginNameResolutionStrategyData(entity.getLoginName())).insert(
+        this.getSqlMapClientTemplate(new SimpleResolutionStrategyData(entity.getLoginName())).insert(
                 "ms-insert-test", entity);
     }
 
     public TestEntity getEntity(String loginName) {
-        return (TestEntity) this.getSqlMapClientTemplate(new LoginNameResolutionStrategyData(loginName))
+        return (TestEntity) this.getSqlMapClientTemplate(new SimpleResolutionStrategyData(loginName))
                 .queryForObject("ms-select-test-by-loginName", loginName);
     }
 
@@ -22,14 +23,14 @@ public class SimpleDAO extends ShardedSqlMapClientDAOSupport {
     public List<TestEntity> getEntities(String[] loginNames) {
         Map<String, String[]> param = new HashMap<String, String[]>(1);
         param.put("loginNames", loginNames);
-        return this.getSqlMapClientTemplate(new LoginNameResolutionStrategyData(loginNames)).queryForList(
+        return this.getSqlMapClientTemplate(new SimpleResolutionStrategyData(loginNames)).queryForList(
                 "ms-select-test-by-loginName-array", param);
     }
     @SuppressWarnings("unchecked")
     public List<TestEntity> getEntitiesSorted(String[] loginNames) {
         Map<String, String[]> param = new HashMap<String, String[]>(1);
         param.put("loginNames", loginNames);
-        return this.getSqlMapClientTemplate(new LoginNameResolutionStrategyData(loginNames)).queryForListSorted(
+        return this.getSqlMapClientTemplate(new SimpleResolutionStrategyData(loginNames)).queryForListSorted(
                 "ms-select-test-by-loginName-array", param , new Comparator(){
 
                     public int compare(Object o1, Object o2) {
@@ -49,7 +50,7 @@ public class SimpleDAO extends ShardedSqlMapClientDAOSupport {
     public Map<String, TestEntity> getEntitiesMap(String[] loginNames) {
         Map<String, String[]> param = new HashMap<String, String[]>(1);
         param.put("loginNames", loginNames);
-        return this.getSqlMapClientTemplate(new LoginNameResolutionStrategyData(loginNames)).queryForMap(
+        return this.getSqlMapClientTemplate(new SimpleResolutionStrategyData(loginNames)).queryForMap(
                 "ms-select-test-by-loginName-array", param, "loginName");
     }
 }
